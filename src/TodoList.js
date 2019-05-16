a9b555d182621b56f2708cc811e46e41b86410f9
winner <1750110604@qq.com>
2019-05-16 16:14:19 +0800

    store

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,20 +3,16 @@ import 'antd/dist/antd.css'
 import {Input, Button, List} from 'antd'
 import store from './store'
 
-const data = [
-  'Racing car into crowd.',
-  'Japanese wed commoner.',
-  'Australian after outback crash.',
-  'Man charged wedding girl.',
-  'Los Angeles huge wildfires.',
-];
-
 class TodoList extends Component {
 
   constructor (props) {
     super(props);
-    this.store = store.getState()
-    console.log(this.store)
+    this.state = store.getState()
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
+    // store里面的数据发生变化后，就会触发
+    store.subscribe(this.handleStoreChange)
   }
 
 
@@ -24,19 +20,57 @@ class TodoList extends Component {
     return (
       <div style={{margin: '10px'}}>
         <div>
-          <Input placeholder = 'todo info' style={{width: '300px', marginRight: '10px'}} />
-          <Button type="primary">提交</Button>
+          <Input
+            placeholder = 'todo info'
+            style={{width: '300px', marginRight: '10px'}}
+            value={this.state.inputValue}
+            onChange={this.handleInputChange}
+          />
+          <Button
+            type="primary"
+            onClick={this.handleBtnClick}
+          >提交</Button>
         </div>
         <List
           style={{marginTop: '10px', width: '300px'}}
           bordered
-          dataSource={this.store.list}
-          renderItem={item => (<List.Item>{item}</List.Item>)}
+          dataSource={this.state.list}
+          renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
         />
       </div>
     )
   }
 
+  // 输入改变事件
+  handleInputChange (e) {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action)
+  }
+
+  // 刷新store变化后的数据
+  handleStoreChange () {
+    this.setState(store.getState())
+  }
+
+  // 提交
+  handleBtnClick () {
+    const action = {
+      type: 'add_todo_item'
+    }
+    store.dispatch(action)
+  }
+
+  // 删除
+  handleItemDelete (index) {
+    const action = {
+      type: 'delete_todo_item',
+      index
+    }
+    store.dispatch(action)
+  }
 }
 
 export default TodoList
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import { createStore } from 'redux'
 import reducer from './reducer'
 
 // 创建数据存储仓库
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store
